d2713eec174f0c17445240c7290a6249eac6349e
espresso-patronum <hungergums@gmail.com>
2019-12-11 13:17:07 +0700

    Updates

---
+++ precache-manifest.52e131611f523781f82de6b4b4f914f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c455326c8aba0643537b6453871f5bf",
+    "url": "/anothercv/index.html"
+  },
+  {
+    "revision": "6acba0aff0ee5c101b3b",
+    "url": "/anothercv/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3f49ddce98a98d96f0ec",
+    "url": "/anothercv/static/js/2.f5ff5e22.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/anothercv/static/js/2.f5ff5e22.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6acba0aff0ee5c101b3b",
+    "url": "/anothercv/static/js/main.de9de178.chunk.js"
+  },
+  {
+    "revision": "79699e7fd1fd36750124",
+    "url": "/anothercv/static/js/runtime-main.c214c2aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anothercv/static/media/logo.5d5d9eef.svg"
+  }
+]);
